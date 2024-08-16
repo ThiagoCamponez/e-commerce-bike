0fd508f32bce556c3313039f2d5f5b5699691951
Camponez <thiagotradercamponez@gmail.com>
2024-08-16 19:54:00 -0300

    Revert "Atualizado para a API correta"
    This reverts commit 3c1e7ca7ff00a9c48353b798285f047615b4409b.

--- src/app/types/ProductType.ts
+++ src/app/types/ProductType.ts
@@ -1,14 +1,8 @@
-export interface ProductType {
-    id: string;
-    images: ImageType[];
-    brand: string;
-    name: string;    
-    price: number;    
-  }
-  
-  export interface ImageType {
-    id: string;
-    url: string;
-    order_id: string;
-  }
-  
+export type ProductType = {
+    id: number;
+    title: string;
+    price: number | null;
+    description: string | null;
+    image: string;
+    category: string;
+}
